85aac4e16bd08fe7e4cc191159e9f99a86aac95a
Filipp <ponf@inbox.ru>
2016-07-25 14:20:15 +0300

    Initial Commit

---
+++ forecast/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  forecast
+//
+//  Created by Филипп Панфилов on 25/07/16.
+//  Copyright © 2016 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ forecast/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  forecast
+//
+//  Created by Филипп Панфилов on 25/07/16.
+//  Copyright © 2016 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
